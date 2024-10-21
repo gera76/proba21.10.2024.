840c064662222aee3950b63c8efd83848ac16618
gera76 <gerasimovicdimitrije7@gmail.com>
2024-10-21 09:09:34 +0200

    kod sa casa

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(a);
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(b);
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine();
